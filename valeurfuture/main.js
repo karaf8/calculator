13ee5816f917c422c8ff980856e3a54e4bc3e278
karaf8 <kas.rafizi8@gmail.com>
2025-03-02 21:36:20 +0100

    Add files via upload

---
+++ valeurfuture/main.js
@@ -0,0 +1,95 @@
+function updateResult() {
+    let interestType = document.getElementById("interestType").value;
+    let timeType = document.getElementById("timeType").value;
+    let rate = parseFloat(document.getElementById("amount").value);
+    let an = parseFloat(document.getElementById("an").value);
+    let moi = parseFloat(document.getElementById("mo").value);
+    let jo = parseFloat(document.getElementById("jo").value);
+    let nom = parseFloat(document.getElementById("va").value);
+    let n = 0;
+
+    let vf = 0;
+
+    console.log(rate)
+    rate/=100;
+
+
+    if(timeType=="annuel"){
+        n+=an;
+        n+=moi/12;
+        n+=jo/360;
+    }
+
+    if(timeType=="semestriel"){
+        n+=an*2;
+        n+=moi/6;
+        n+=jo/180;
+        rate/=2;
+    }
+
+    if(timeType=="trimestriel"){
+        n+=an*4;
+        n+=moi/3;
+        n+=jo/90;
+        rate/=4;
+    }
+
+    if(timeType=="mensuel"){
+        n+=an*12;
+        n+=moi;
+        n+=jo/30;
+        rate/=12;
+    }
+
+    if(timeType=="quotidien"){
+        n+=an*360;
+        n+=moi*30;
+        n+=jo;
+        rate/=360;
+    }
+
+
+
+    console.log(n)
+
+    if (interestType === "simple") {
+        console.log("Calculating Simple Interest...");
+        vf = nom*(1+n*rate);
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // Add your simple interest calculation logic here
+    } else if (interestType === "compose") {
+        console.log("Calculating Compound Interest...");
+        vf = nom*Math.pow(1+rate, n);
+
+
+
+
+
+
+
+        // Add your compound interest calculation logic here
+    }
+
+    console.log(vf)
+
+
+    let resultBox = document.getElementById("result");
+    resultBox.textContent = vf;
+    resultBox.classList.add("show");
+    setTimeout(() => resultBox.classList.remove("show"), 500);
+
+}
